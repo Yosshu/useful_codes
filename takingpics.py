ec64bf4dfb3b93931fb01af33c784d6da16a8956
Yosshu <100824513+Yosshu@users.noreply.github.com>
2022-08-09 00:43:16 +0900

    Aで写真撮影

---
+++ takingpics.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+
+#カメラの設定　デバイスIDは0
+cap = cv2.VideoCapture(0)
+count = 0
+#繰り返しのためのwhile文
+while True:
+    #カメラからの画像取得
+    _, frame = cap.read()
+
+    #カメラの画像の出力
+    cv2.imshow('camera', frame)
+
+    #繰り返し分から抜けるためのif文
+    key = cv2.waitKey(1)
+    if key == ord('a'):                 # 写真撮影
+        cv2.imwrite(f'pic{count}.png',frame)
+        count += 1
+    elif key == 27:   #Escで終了
+        break
+
+
+#メモリを解放して終了するためのコマンド
+cap.release()
+cv2.destroyAllWindows()
