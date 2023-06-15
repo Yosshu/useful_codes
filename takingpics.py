b37760e519c5bc2d2eb4afbea4c172b898694b1b
Yosshu <100824513+Yosshu@users.noreply.github.com>
2023-06-15 19:12:57 +0900

    幅と高さの変更
    取得する画像の幅と高さの変更ができるようになった．

--- takingpics.py
+++ takingpics.py
@@ -4,11 +4,15 @@ import cv2
 #カメラの設定　デバイスIDは0
 cap = cv2.VideoCapture(0)
 count = 0
+
+# カメラの解像度を設定
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # 幅の設定
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)  # 高さの設定
+
 #繰り返しのためのwhile文
 while True:
     #カメラからの画像取得
     _, frame = cap.read()
-
     #カメラの画像の出力
     cv2.imshow('camera', frame)
 
